03f2d16cd813cbd6ee243f1278b7413818f0c3de
Anthony <anthony.bellier@laposte.net>
2022-05-03 10:09:09 +0200

    Recuperation de valeur dans des input généré par l'utilisateur lui même

--- exo_13.php
+++ exo_13.php
@@ -13,13 +13,36 @@
         <h1>Combien vous désirez ajouter de compétences ?</h1>
         <form action="exo_13.php" method="post">
             <input type="text" name="choix">
-            <input type="submit" name="soumettre1" value="Soumettre">
+            <input type="submit" name="envoyer" value="Soumettre">
         </form>
    </section>
 
    <section>
        <h1>Ajouter vos compétences</h1>
+       <form action="exo_13.php" method="post">
+            <?php
+            if(isset($_POST['envoyer'])){
+                $nb = $_POST['choix'];
+                for($i=1;$i<=$nb;$i++){
+                    echo("Compétences $i : ");
+                    echo("<input type='text' name='choix[]'/><br/>");
+                }
+            }
+            ?>
+            <input type="submit" value="Soumettre" name="soumettre" />
+       </form>
    </section>
-   
+   <section>
+       <?php
+          if(isset($_POST['soumettre'])){
+            //  print_r($_POST['choix']); Affiche les valeurs
+            $nb=count($_POST['choix']);
+            for($i=0;$i<$nb;$i++){
+                echo($_POST['choix'][$i]."<br/>");
+            }
+          }
+       ?>
+   </section>
+
 </body>
 </html>
