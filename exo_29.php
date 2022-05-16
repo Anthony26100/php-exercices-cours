44dbf94e0c01bdbaa81cabb9bcbd4f0f58643d21
Anthony <anthony.bellier@laposte.net>
2022-05-16 10:24:25 +0200

    differents foreach

--- exo_29.php
+++ exo_29.php
@@ -6,12 +6,13 @@
         
     };
 
+    echo str_repeat('__',84).'<br/>';
 
     foreach($nombres as $nombre){ // affiche uniquement les valeurs sont on ne veut pas l'indice 
         echo($nombre.'<br/>');
     }
 
-
+    echo str_repeat('__',84).'<br/>';
     $capitales=[['France','Paris'],['ITALIE','Rome']];
     // Avec la fonction list()
     foreach($capitales as list($pays,$ville)){
@@ -22,17 +23,16 @@
     foreach($capitales as [$pays,$ville]){
         echo $pays.' => '.$ville.'<br/>';
     }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FOR EACH / List()</title>
-</head>
-<body>
-    <h1>Accéder à un élément d'un tableau</h1>
-</body>
-</html>
+    echo str_repeat('__',84).'<br/>';
+
+    $capitales=[['Pays'=>'France','Ville'=>'Paris'],['Pays'=>'ITALIE','Ville' => 'Rome']];
+    foreach($capitales as list('Pays' => $pays,'Ville' => $ville)){
+        echo $pays.' => '.$ville.'<br/>';
+    }
+
+    echo str_repeat('__',84).'<br/>';
+
+
+
+?>
