762e0ee27ccd39fdd9eac17a7438d95f47faae6d
Anthony <anthony.bellier@laposte.net>
2022-05-16 18:06:02 +0200

    exo-24 modif

--- exo-24.php
+++ exo-24.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="FR-fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/main_connexion.css">
+    <title>Connection or Not</title>
 </head>
 <body>
-    <form action="exo-24.php" method="post">
-        <input type="text" name="pseudo" value="Pseudo">
-        <input type="password" name="password" value="Password">
-        <input type="submit" name="envoyer" value="Ce connecter">
-    </form>
-
+    <section>
+        <form action="exo-24.php" method="post">
+            <input type="text" name="pseudo" value="Pseudo">
+            <input type="password" name="password" value="Password">
+            <input type="submit" name="envoyer" value="Ce connecter">
+        </form>
+    </section>
     <?php
 
 
@@ -33,6 +35,8 @@
             //     header("Location: exo-24.php");
             // }
         }
+
+        
     ?>
 </body>
 </html>
