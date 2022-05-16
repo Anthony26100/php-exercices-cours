e11804210cd14dec5f8386790c3d8fe21ff90082
Anthony <anthony.bellier@laposte.net>
2022-05-16 12:06:23 +0200

    create include and require php pages

---
+++ exo_33.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Methode Require & Include</title>
+</head>
+<body>
+    <form action="" method="">
+        <label for="username">Pseudo</label>
+        <input type="text" name="username"/>
+        <input type="submit" value="Soumettre" name="soumettre"/>
+    </form>
+
+    <a href="33-1-require.php">Require</a>
+    <a href="33-2-include.php">Include</a>
+</body>
+</html>
