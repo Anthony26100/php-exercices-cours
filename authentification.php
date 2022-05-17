9ef6b13561a2c124c82b5133bccacd04cb1b906b
Anthony <anthony.bellier@laposte.net>
2022-05-17 17:17:33 +0200

    authentification ON/OFF

--- authentification.php
+++ authentification.php
@@ -4,14 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/connexion.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>BANNI</title>
 </head>
 <body>
     <h1>VOUS ETES BANNI !</h1>
     <button onclick="history.back()">Retour</button>
-    <?php
-    echo "<br/>".$_COOKIE['user'];
-    ?>  
 </body>
 </html>
